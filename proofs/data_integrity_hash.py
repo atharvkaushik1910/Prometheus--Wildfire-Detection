09ef799f6444e557ad6353dda591b8e9c903188f
atharvkaushik1910 <84839739+atharvkaushik1910@users.noreply.github.com>
2025-05-30 17:04:59 +0530

    Update data_integrity_hash.py

--- proofs/data_integrity_hash.py
+++ proofs/data_integrity_hash.py
@@ -1,1 +1,11 @@
+import hashlib
+
+def generate_hash(data: str) -> str:
+    return hashlib.sha256(data.encode()).hexdigest()
+
+original_data = "temperature=45;humidity=20;wind=30"
+tampered_data = "temperature=50;humidity=20;wind=30"
+
+print("Original Hash:", generate_hash(original_data))
+print("Tampered Hash:", generate_hash(tampered_data))
 
