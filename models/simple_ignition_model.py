966f97e0d78c8d6dded07c2e87a629eb3ef91b71
atharvkaushik1910 <84839739+atharvkaushik1910@users.noreply.github.com>
2025-05-30 17:17:21 +0530

    Update simple_ignition_model.py

--- models/simple_ignition_model.py
+++ models/simple_ignition_model.py
@@ -1,1 +1,13 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+
+# Defining model architecture
+model = Sequential([
+    Dense(16, activation='relu', input_shape=(4,)),
+    Dense(8, activation='relu'),
+    Dense(1, activation='sigmoid')
+])
+
+# Summary of model
+model.summary()
 
